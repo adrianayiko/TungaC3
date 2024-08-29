aa5e7e7f052c3ea43f84d7df6e9b7b2fc26b2f18
adrianayiko <ayikoadrian4@gmail.com>
2024-08-29 18:27:12 +0300

    Template inherittance and dynamic routing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    names = 'mark'
+    return render_template('about.html', autor = names)
+
+@app.route('/hello', methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template('form.html', name=name)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
