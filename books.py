954c7dfb44ec4bb9dc8b41491c2a794fbf89c876
adrianayiko <ayikoadrian4@gmail.com>
2024-09-04 21:12:30 +0300

    Dummy data and static file design

---
+++ books.py
@@ -0,0 +1,28 @@
+articles = [
+    {
+        'author': 'John Doe',
+        'title': 'Introduction to Python',
+        'details': 'This article provides an introduction to the Python programming language.',
+    },
+    {
+        'author': 'Jane Smith',
+        'title': 'Web Development with Flask',
+        'details': 'Learn how to build web applications using the Flask framework.',
+    },
+    {
+        'author': 'Mike Johnson',
+        'title': 'Data Analysis with Pandas',
+        'details': 'Explore the powerful data manipulation capabilities of the Pandas library.',
+    },
+    {
+        'author': 'Sarah Williams',
+        'title': 'Machine Learning Basics',
+        'details': 'Get started with the fundamentals of machine learning and its applications.',
+    },
+    {
+        'author': 'David Brown',
+        'title': 'Introduction to Django',
+        'details': 'Learn how to build web applications using the Django framework.',
+    }
+]
+
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
+from books import articles
 
 app = Flask(__name__)
 
-#code to render temlates of Html 
+#code to render templates of Html 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
-    names = 'mark'
-    return render_template('about.html', autor = names)
+    return render_template('about.html', articles = articles)
 
 @app.route('/hello', methods=["POST"])
 def hello():
