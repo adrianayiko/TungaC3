5d32d654ac2c70a7be69f70ee399b252a8d4a376
adrianayiko <ayikoadrian4@gmail.com>
2024-09-05 17:21:26 +0300

    Refinements to thge Blog

--- main.py
+++ main.py
@@ -11,11 +11,25 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html', articles = articles)
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-@app.route('/hello', methods=["POST"])
-def hello():
-    name = request.form.get("name")
-    return render_template('form.html', name=name)
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    #logic to create a new article 
+    if request.method == "GET":
+        return render_template('form.html')
+    if request.method == "POST":
+        #retrieve data from form
+        author = request.form['author']
+        title = request.form['title']
+        details = request.form['details']
+        
+        #add article to list 
+        articles.append({"title": title, "author": author, "details":details})
+        
+    return render_template('form.html')
     
 
 if __name__ == '__main__':
