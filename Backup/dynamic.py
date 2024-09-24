d54c1a73b938579f8907467fab9c211e8613f336
adrianayiko <ayikoadrian4@gmail.com>
2024-09-24 14:29:54 +0300

    gitignore and deleted files during refactor

---
+++ Backup/dynamic.py
@@ -0,0 +1,9 @@
+from app import Flask
+app = Flask(__name__)
+
+#define a dynamic route for veiwing blog posts by post ID
+
+@app.route('/post/<int:post_id>')
+def view_post(post_id):
+    #logic t retrieve  and display blog post
+    return f"Viewing Blog Post #{post_id}"
---
+++ Backup/server.py
@@ -0,0 +1,15 @@
+from app import Flask 
+app = Flask(__name__)
+
+# route for "/"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+#route for "/about"
+@app.route('/about')
+def about():
+    return 'The about page'
+
+if __name__ == '__main__':
+    app.run()
