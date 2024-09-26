7e4fa14447b88bc6da8921383ecd197b3c2d6e75
adrianayiko <ayikoadrian4@gmail.com>
2024-09-26 10:17:00 +0300

    Repated files in branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# TungaC3
-My Repo for my back end python development in Tunga-cohort 
--- blogsite/app/blog/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-blog = Blueprint('blog', __name__,template_folder="templates")
-
-from . import views
